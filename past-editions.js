29a8d3869c4be1fd11d638843d132110ed97fb2f
Govind Jeevan <govindjeevan7@gmail.com>
2018-10-23 17:44:33 +0530

    Speakers list done

---
+++ past-editions.js
@@ -0,0 +1,261 @@
+var speakers2012 = [
+  
+{
+    "name": "Arun",
+    "image": "assets/img/Speaker Pictures/2012/Arun.jpeg",
+    "designation": ""
+},
+
+{
+    "name": "Bala Girisaballa",
+    "image": "assets/img/Speaker Pictures/2012/Bala Girisaballa.jpeg",
+    "designation": ""
+
+},
+
+{
+    "name": "Brodha V",
+    "image": "assets/img/Speaker Pictures/2012/Brodha V.jpg",
+    "designation": ""
+
+},
+
+{
+    "name": "Dasami",
+    "image": "assets/img/Speaker Pictures/2012/Dasami.jpg",
+    "designation": ""
+
+},
+
+{
+    "name": "Gopal",
+    "image": "assets/img/Speaker Pictures/2012/Gopal.jpg",
+    "designation": ""
+},
+
+{
+    "name": "Osama",
+    "image": "assets/img/Speaker Pictures/2012/Osama.jpg",
+    "designation": ""
+},
+{
+    "name": "Shruti",
+    "image": "assets/img/Speaker Pictures/2012/Shruti.jpg",
+    "designation": ""
+},
+{
+    "name": "Sumit",
+    "image": "assets/img/Speaker Pictures/2012/Sumit.jpg",
+    "designation": ""
+},
+
+{
+    "name": "Vineeth",
+    "image": "assets/img/Speaker Pictures/2012/Vineeth.jpg",
+    "designation": ""
+},
+
+
+]
+
+
+
+var speakers2011 = [
+  
+    {
+        "name": "Anoj",
+        "image": "assets/img/Speaker Pictures/2011/Anoj.jpeg",
+        "designation": ""
+    },
+
+    {
+        "name": "Arvind",
+        "image": "assets/img/Speaker Pictures/2011/Arvind.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Bhavna",
+        "image": "assets/img/Speaker Pictures/2011/Bhavna.jpeg",
+        "designation": ""
+    },
+
+    {
+        "name": "John Louis",
+        "image": "assets/img/Speaker Pictures/2011/John Louis.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Kumaresh",
+        "image": "assets/img/Speaker Pictures/2011/Kumaresh.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Mohandas",
+        "image": "assets/img/Speaker Pictures/2011/Mohandas.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Narendra",
+        "image": "assets/img/Speaker Pictures/2011/Narendra.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Poonam",
+        "image": "assets/img/Speaker Pictures/2011/Poonam.jpeg",
+        "designation": ""
+    },
+
+    {
+        "name": "Prasanta",
+        "image": "assets/img/Speaker Pictures/2011/Prasanta.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Rishikesha",
+        "image": "assets/img/Speaker Pictures/2011/Rishikesha.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Rudramuni B",
+        "image": "assets/img/Speaker Pictures/2011/Rudramuni B.jpg",
+        "designation": ""
+    },
+
+
+    {
+        "name": "Sachin Jain",
+        "image": "assets/img/Speaker Pictures/2011/Sachin Jain.jpg",
+        "designation": ""
+    },
+
+
+    {
+        "name": "Vinnel",
+        "image": "assets/img/Speaker Pictures/2011/Vineel.jpg",
+        "designation": ""
+    },
+    {
+        "name": "Visalini",
+        "image": "assets/img/Speaker Pictures/2011/Visalini.jpg",
+        "designation": ""
+    },
+
+
+    
+    ]
+
+
+
+var speakers2016 = [
+
+    {
+        "name": "Ashok Rao",
+        "image": "assets/img/Speaker Pictures/2016/Ashok Rao.jpeg",
+        "designation": ""
+    },
+
+    {
+        "name": "Binoy John",
+        "image": "assets/img/Speaker Pictures/2016/Binoy John.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Rajeswari-Vaidyanathan",
+        "image": "assets/img/Speaker Pictures/2016/Rajeswari-Vaidyanathan.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Robin Chaurasiya",
+        "image": "assets/img/Speaker Pictures/2016/Robin Chaurasiya.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Sammilan Shetty",
+        "image": "assets/img/Speaker Pictures/2016/Sammilan Shetty .jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Shantala Hegde",
+        "image": "assets/img/Speaker Pictures/2016/Shantala Hegde.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Sriram Ramaswamy",
+        "image": "assets/img/Speaker Pictures/2016/Sriram Ramaswamy.jpg",
+        "designation": ""
+    },
+
+
+    {
+        "name": "Tim Poston",
+        "image": "assets/img/Speaker Pictures/2016/Tim Poston.jpg",
+        "designation": ""
+    },
+        
+        ]
+    
+var speakers2018 = [
+  
+    {
+        "name": "Aakash Gaurav",
+        "image": "assets/img/Speaker Pictures/2018/Aakash Gaurav.jpeg",
+        "designation": ""
+    },
+
+    {
+        "name": "Ashrita Vemuganti ",
+        "image": "assets/img/Speaker Pictures/2018/Ashrita Vemuganti .jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Chakshu Gupta",
+        "image": "assets/img/Speaker Pictures/2018/Chakshu Gupta.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Charan Singh",
+        "image": "assets/img/Speaker Pictures/2018/Charan Singh.JPG",
+        "designation": ""
+    },
+
+    {
+        "name": "Michael Foley",
+        "image": "assets/img/Speaker Pictures/2018/Michael Foley.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Revati Roy",
+        "image": "assets/img/Speaker Pictures/2018/Revati-Roy.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Sridhar-Rangayan",
+        "image": "assets/img/Speaker Pictures/2018/Sridhar-Rangayan.jpg",
+        "designation": ""
+    },
+
+    {
+        "name": "Sriram Gopal",
+        "image": "assets/img/Speaker Pictures/2018/Sriram Gopal.jpg",
+        "designation": ""
+    },
+            
+        ]
+        
+        

867e04bf1714a34e8bf4fc48ef35a57906063543
Bharath Adikar <lkcbharath@gmail.com>
2019-10-07 23:48:13 +0530

    Basic active videos

--- assets/js/video-centre.js
+++ assets/js/video-centre.js
@@ -11,23 +11,36 @@ $(document).ready(function () {
             scrollLeft: "-=336"
         }, 750);
     });
+
+    
 });
 
 
 //display the video player based on the tab clicked
 function openSeries(evt, vidName) {
-var i, tabcontent, tablinks;
-tabcontent = document.getElementsByClassName("tabcontent");
-for (i = 0; i < tabcontent.length; i++) {
-  tabcontent[i].style.display = "none";
-}
-tablinks = document.getElementsByClassName("tablinks");
-for (i = 0; i < tablinks.length; i++) {
-  tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
-document.getElementById(vidName).style.display = "block";
-evt.currentTarget.className += " active";
+    var i, tabcontent, tablinks;
+    tabcontent = $(".tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = $(".tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(vidName).style.display = "block";
+    evt.currentTarget.className += " active";
+    
 }
 
 // Set default to 2019
 document.getElementById("defaultOpen").click();
+
+$(".vid-item .desc").click(function () {
+    $(this).parent().siblings().removeClass("vid-item-active");
+    $(this).parent().addClass("vid-item-active");
+});
+
+$(".vid-item .thumb").click(function () {
+    $(this).parent().siblings().removeClass("vid-item-active");
+    $(this).parent().addClass("vid-item-active");
+});

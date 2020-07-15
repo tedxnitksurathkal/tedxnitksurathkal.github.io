40b027c7181f57f891d876bc64847c2bfdd36ed0
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-07-16 00:25:01 +0530

    Add Salon page

---
+++ assets/js/salon-faq-list.js
@@ -0,0 +1,42 @@
+var faq_questions = [
+  [
+    'What is a Salon?', 
+    'A salon is an intimate discussion which delves deeply into one subject area. The attendees are invited to interact and connect with the speakers as well as with each other to build the discussion and introspect on contrasting viewpoints. These are the events conducted to keep the TEDx Community engaged and connected.'
+  ],
+  [
+    'Who is this event for?', 
+    'This event will be open to the complete NITK Community, particularly those eager to stimulate their mind with a powerful and engaging discussion, as well as individuals who enjoy watching TEDx videos in their spare time.'
+  ],
+  [
+    'When will the event be held?', 
+    'The TEDxNITKSurathkalSalon will be held on August 1st at 5:00 pm IST.'
+  ],
+  [
+    'How do I register for this event?', 
+    'The link for registering as an attendee to this salon will be shared soon!'
+  ],
+  [
+    'Can I register for anyone else?', 
+    'You can only register for yourself'
+  ],
+  [
+    'How many registrations are available?', 
+    'A maximum of 100 attendees only are allowed in one TEDxNITKSurathkalSalon event.'
+  ],
+  [
+    'What is the platform used for this event?', 
+    'Since this is a virtual event, we will be using Google Meet to conduct this event.'
+  ],
+  [
+    'What is the programme structure for this Salon?', 
+    'The structure will be as follows:<br/>- Talks conducted by the speaker(s) based on the theme.<br/>- Q&A session where the attendees are open to discuss and connect.<br/>- Discussion held by the students of NITK<br/>'
+  ],
+  [
+    'As an attendee, am I permitted to ask questions to the speaker?', 
+    'Yes, there will be a Q&A session held once the speaker has completed their talk in which the attendees are invited to ask enquiries and discuss with the speaker, with regard to the theme of the Salon'
+  ],
+  [
+    'How long is this event going to be?', 
+    'This event will take place for approximately two hours.'
+  ]
+]

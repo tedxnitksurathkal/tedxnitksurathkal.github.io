fa41f9aa545b34ab4cb3ce1fbc84deeedadb8425
Sriram Rao <sriramudupi19@gmail.com>
2020-07-20 13:19:49 +0530

    Fix styles for link

--- assets/js/salon-faq-list.js
+++ assets/js/salon-faq-list.js
@@ -1,50 +1,50 @@
 var faq_questions = [
   [
-    'What is a Salon?', 
+    'What is a Salon?',
     'A salon is an intimate discussion which delves deeply into one subject area. The attendees are invited to interact and connect with the speakers as well as with each other to build the discussion and introspect on contrasting viewpoints. These are the events conducted to keep the TEDx Community engaged and connected.'
   ],
   [
-    'Who is this event for?', 
+    'Who is this event for?',
     'This event will be open to the complete NITK Community, particularly those eager to stimulate their mind with a powerful and engaging discussion, as well as individuals who enjoy watching TEDx videos in their spare time. The event will be streamed live on Youtube as well.'
   ],
   [
-    'What is the theme for TEDxNITKSurathkalSalon?', 
+    'What is the theme for TEDxNITKSurathkalSalon?',
     'The theme for this event will be Atma Nirbhar Bharat which symbolizes a Self-Reliant India.'
   ],
   [
-    'Where can I obtain more information with regard to the theme?', 
-    'Click on the link below for more information on the theme <a href="/theme">Atma-Nirbhar</a>'
+    'Where can I obtain more information with regard to the theme?',
+    'Click on the link below for more information on the theme <a href="/theme" style="display:inline; font-size:14px;">Atma-Nirbhar</a>'
   ],
   [
-    'When will the event be held?', 
+    'When will the event be held?',
     'The TEDxNITKSurathkalSalon will be held on August 1st at 5:00 pm IST.'
   ],
   [
-    'How do I register for this event?', 
+    'How do I register for this event?',
     'The link for registering as an attendee to this salon will be shared soon!'
   ],
   [
-    'Can I register for anyone else?', 
+    'Can I register for anyone else?',
     'You can only register for yourself'
   ],
   [
-    'How many registrations are available?', 
+    'How many registrations are available?',
     'A maximum of 100 attendees only are allowed in one TEDxNITKSurathkalSalon event.'
   ],
   [
-    'What is the platform used for this event?', 
+    'What is the platform used for this event?',
     'Since this is a virtual event, we will be using Google Meet to conduct this event. It will also be streamed on Youtube live.'
   ],
   [
-    'What is the programme structure for this Salon?', 
+    'What is the programme structure for this Salon?',
     'The structure will be as follows:<br/>- Talks conducted by the speaker(s) based on the theme.<br/>- Q&A session where the attendees are open to discuss and connect.<br/>- Discussion between the speakers and attendees.'
   ],
   [
-    'As an attendee, am I permitted to ask questions to the speaker?', 
+    'As an attendee, am I permitted to ask questions to the speaker?',
     'Yes, there will be a Q&A session held once the speaker has completed their talk in which the attendees are invited to ask enquiries and discuss with the speaker, with regard to the theme of the Salon'
   ],
   [
-    'How long is this event going to be?', 
+    'How long is this event going to be?',
     'This event will take place for approximately two hours.'
   ]
 ]

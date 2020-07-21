299d0f6b13b1cc288ec15e8ff2324e91e0c1ee78
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-07-22 01:08:14 +0530

    Drop google map
    We no longer display a map on our contact page. Let's remove associated
    initialization code and library.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@
  /* ------------------------------------- */
 /*   PRE LOADING                          */
 /*   sliders                      */
-/*    MAPS                               */
-/*   COUNTER JS              */
 
 
 
@@ -31,49 +29,4 @@ $(document).ready(function() {
             $('.header').addClass('sticky_header');
         }
     });
-
-
-
-        /* --------------------------------------------------------
-    MAPS
-    ----------------------------------------------------------- */
-    var map = $('#map');
-    if(map.length > 0) {
-        google.maps.event.addDomListener(window, 'load', init);
-        var lattuide = map.attr('data-lat');
-        var longtuided = map.attr('data-lon');
-    }
-    function init() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-        var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
-            zoom: 16,
-            scrollwheel: false,
-            navigationControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-            // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(lattuide, longtuided), // New York
-
-            // How you would like to style the map.
-            // This is where you would paste any style found on Snazzy Maps.
-            styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
-        };
-
-        // Get the HTML DOM element that will contain your map
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
-
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
-
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lattuide, longtuided),
-            map: map,
-            title: 'TEDxNITKSurathkal!'
-        });
-    }
-
 });

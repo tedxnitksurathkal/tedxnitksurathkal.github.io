9e3c306a9b8fe2bbf6072e6bd2f75cf8f9ae8678
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-08-03 09:13:13 +0000

    Load fewer resources (#91)
    * Drop counterup, countdown js libraries
    
    Counterup and Countdown libraries are used for the "countdown until big
    event" section on the index page. Loading the libraries on other pages
    is wasteful.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop wow js library
    
    Wow is used to reveal animations as we scroll. While it might have had a
    purpose, we no longer use any of effects.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop waypoint js library
    
    Waypoints is the easiest way to trigger a function when you scroll to an
    element. The waypoint is used for speakers.html, which itself is no
    longer used.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop owl carousel
    
    Owl carousel is used in index page to generate "past speakers" carousel
    and has no use in other pages.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Serve popper through CDN
    
    Popper is one of js required for bootstrap. Instead of serving locally,
    let's use CDN distribution.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop google map
    
    We no longer display a map on our contact page. Let's remove associated
    initialization code and library.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop et-line
    
    et-line is an icon library, which is no longer used.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop animate.css
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop font-awesome for ionicons
    
    We serve font-awesome locally while requesting ionicons. Having two
    different icon libraries is redundant, and since font-awesome also
    requires linearicons, let's drop it.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop Monsterrat font weights 500, 900
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Drop parallax library
    
    The parallax effect is easy to replicate using css. Let's drop the
    external library and add the effect using css.
    
    We crop the assets/img/bg/team.jpg, to make it look better.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Upgrade to jquery 3.5.1
    
    Due to dependency on parallax.js, many pages used jquery-2.4.1 with
    index including both jquery-2.4.1 and jquery-3.4.1. As parallax.js is
    replaced with css, let's upgrade the jquery version as well.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * Merge lazy_load.js and main.js
    
    Lazy load is included in each of the pages. By merging main and lazy
    load scripts, we can reduce 1 request for each page.
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>
    
    * remove unused js files
    
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,33 @@
-/* ------------------------------------- */
-/*  TABLE OF CONTENTS
- /* ------------------------------------- */
-/*   PRE LOADING                          */
-/*   WOW                                 */
-/*   sliders                      */
-/*    MAPS                               */
-/*   COUNTER JS              */
-
-
-
-    /* ==============================================
-/*  PRE LOADING
-  =============================================== */
+// Loader
 'use strict';
-$(window).load(function() {
+$(window).on('load', function() {
     $('.loader').delay(100).fadeOut('slow');
 });
 
 
 $(document).ready(function() {
-
     'use strict';
-    /* ==============================================
-     /*   wow
-      =============================================== */
-    var wow = new WOW(
-        {
-            animateClass: 'animated',
-            offset: 10,
-            mobile: true
+
+    // Sticky header
+    let header = document.querySelector('.header');
+    
+    window.addEventListener('scroll', function () {
+      if (window.scrollY < 100)
+        header.classList.remove('sticky_header');
+      else
+        header.classList.add('sticky_header');
+    });
+
+    // Lazy load images
+    $('.lazy').lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        afterLoad: function(element) {
+          element.css('background-image', 'none');
+        },
+        onError: function(element) {
+          console.log('Error loading ' + element.data('src'));
         }
-    );
-    wow.init();
-    /* ==============================================
-        STICKY HEADER
-        =============================================== */
-
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() < 100) {
-            $('.header').removeClass('sticky_header');
-        } else {
-            $('.header').addClass('sticky_header');
-        }
     });
-
-    /* ==============================================
-     SLIDER
-     =============================================== */
-    $(".cover_slider").owlCarousel({
-        loop:true,
-        autoplay:true,
-        smartSpeed:1000,
-        autoplayHoverPause:false,
-        dots:true,
-        nav:false,
-        items:1,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        dotsContainer: '.cover_dots'
-    });
-    /* ------------------------------------- */
-    /* Animated progress bars
-     /* ------------------------------------- */
-
-    var waypoints = $('.progress_container').waypoint(function() {
-        $('.progress .progress-bar').progressbar({
-            transition_delay: 1000
-        });
-    },{
-        offset: '50%'
-    });
-
-
-        /* --------------------------------------------------------
-    MAPS
-    ----------------------------------------------------------- */
-    var map = $('#map');
-    if(map.length > 0) {
-        google.maps.event.addDomListener(window, 'load', init);
-        var lattuide = map.attr('data-lat');
-        var longtuided = map.attr('data-lon');
-    }
-    function init() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-        var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
-            zoom: 16,
-            scrollwheel: false,
-            navigationControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-            // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(lattuide, longtuided), // New York
-
-            // How you would like to style the map.
-            // This is where you would paste any style found on Snazzy Maps.
-            styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
-        };
-
-        // Get the HTML DOM element that will contain your map
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
-
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
-
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lattuide, longtuided),
-            map: map,
-            title: 'TEDxNITKSurathkal!'
-        });
-    }
-
 });

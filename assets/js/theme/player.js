8b39998cedbb1cc2c5810e28017391ae67358652
gaurang2001 <gaurangvelingkar@gmail.com>
2020-10-07 16:21:09 +0530

    feat: theme reveal upon finish

--- assets/js/theme/player.js
+++ assets/js/theme/player.js
@@ -4,12 +4,18 @@ AFRAME.registerComponent("player", {
         this.el.addEventListener('collide', (evt) => {
             if (evt.detail.body.el.id != undefined && evt.detail.body.el.id == "finish-tile") {
                 this.el.sceneEl.exitVR()
-
-                let finishDlgElm = document.querySelector("#maze-full")
-                finishDlgElm.setAttribute("style", "display: none");
+                
+                $('#maze-full').fadeOut(500);
+                animate_trailblazers();
             }
         });
     }
 })
 
-
+function animate_trailblazers() {
+    $('.has-animation').each(function(index) {
+            $(this).delay($(this).data('delay')).queue(function(){
+            $(this).addClass('animate-in');
+        });
+    });
+}

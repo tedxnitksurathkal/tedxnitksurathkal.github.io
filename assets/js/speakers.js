5b9ccebeaf7a44ca07a7f121a7d7dabd7a4d236f
Abhishek Kumar <abhishekkumar8222@gmail.com>
2021-02-05 15:25:41 +0530

    Update first batch of speakers for 2021 (#124)

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,44 +1,33 @@
-var speakers=[
-    {
-        name: "Vishak Nair",
-        title: "Engineer turned Actor",
-        description: [
-            "For those who aren’t just defined by their designations are the ones who set trails for others to follow. Had life not blossomed on the silver screen, Vishak Nair would have perhaps been busy with machines at a swanky corporate office. Vishak Nair is an Indian film and theatre actor, who is known for his work in the Malayalam industry. He made his acting debut in the coming-of-age romantic drama, Aanandam (2016) directed by Ganesh Raj which made him a household name. Vishak has starred in numerous movies following his debut endeavour. He is the also the Chief Operating Officer and Director of MadGenius productions.",
-            "Vishak pursued Mechanical Engineering from National Institute of Technology, Karnataka (NITK) and graduated in 2014. During his time in NITK he actively took part in clubs such as NITK Music Club, Rotaract NITK, Dramatics Team and served as the President of the Toastmasters Club. Vishak worked for 2 years at Daimler India Commercial Vehicles in Chennai when he finally found his call into acting. Enough brilliance proving that he is the jack of all trades and master of many too."          
-        ],
-        show_after: "2020-10-12T09:30:00+0000",
-        img_link: "vishak.jpeg"
-    },
-    {
-        name: "Sonia Thiyam",
-        title: "Doctor turned Fashion Blogger",
-        description: [
-            "A doctor turned fashion blogger and digital content creator, Sonia Thiyam is currently taking the world by storm with her excellent fashion sense and courage to meet society head first. She did her MBBS and then took a bold move to get into blogging. In a world filled with people, stuck in a perpetual rat race dictated by what society expects, she took the road less taken by pursuing her passion. She built her image, taking one hurdle at a time, and is now an established name in the fashion industry.",
-            "Sonia Thiyam currently creates beautiful, artistic looks and has worked with huge brands such as Balmain and has curated multiple looks for them. The impact her work creates portrays her talent and her ability to create influential art out of the most subtle elements. Battling stereotypes and changing people’s perceptions regarding her heritage, she stands as a massive inspiration for stereotyped victims to realize their worth. She strongly believes one must follow and live their dreams, no matter what the hurdles are."          
-        ],
-        show_after: "2020-10-13T09:30:00+0000",
-        img_link: "sonia.jpeg"
-    },
-    {
-        name: "Adhitya Iyer",
-        title: "Engineer turned Writer",
-        description: [
-            "“Ïf engineering were a religion, it would be the 5th most populous religion in India.”",
-            "A statement that comes as no surprise. By virtue of his academic ability, he enrolled as an engineering student. But it didn’t take him long to realize there were other callings worth fulfilling, and so he did. After founding his own campus T-shirt brand Annanymous, coining the term \"Ghanta Engineer\" and even selling chai in India’s silicon valley he realized that his story was one shared by millions. In true folktale fashion, he quit his job and set out to unravel the stories of India’s engineers.",
-            "Two years of exploration and a beard later he published “The Great Indian Obsession” a bestseller and his claim to fame today. “I set on a backpacking trip across the country to unravel the massive phenomenon at the end of which I lost a camera full of images, but I found a story to tell the world.” Adhitya Iyer is truly a curious and progressive mind, set out on fixing the education system."          
-        ],
-        show_after: "2020-10-14T09:30:00+0000",
-        img_link: "adhitya.jpeg"
-    },
-    {
-        name: "Bipin Dhane",
-        title: "Social Worker",
-        description: [
-            "A graduate of IIT Kharagpur, Bipin Dhane quit his corporate job to pursue his true calling of educating children. He came to the remote river island of Majuli to teach underprivileged children and later set up a school in the village of Kulamua at the request of the villagers. Armed with the help of the villagers who donated their lands, wood and manpower, the Hummingbird School was built.",
-            "Today, Dhane serves as principal at the school for underprivileged kids from the indigenous Mising tribe. Sensing the need to extend their work with the community, along with his colleagues, he founded the Ayang trust. It works towards not only improving the education of the children there but also uplifting the local community by promoting a producer’s collective and working with women weavers to let them earn a livelihood."          
-        ],
-        show_after: "2020-10-15T09:30:00+0000",
-        img_link: "bipin.jpeg"
-    }
+var speakers = [
+  {
+    name: "Padmini Rout",
+    title: "Chess Grandmaster | IM & WGM",
+    description: [
+      "A young 26 year old who has already bagged the title of 'International Master and' and 'Women  Grandmaster', Padmini Rout has sealed her name as one of the most remarkable chess players in the world. Her father introduced her to the game when she was only 9 and it has been her passion since then. A Potterhead and Star Wars fan, she is one of the most relatable speakers of this edition.",
+      "Ms. Rout has won innumerable competitions and received the Biju Patnaik Sports Award in 2007 and Eklavya Award in 2009. She is a four-time National Women's Primier title holder from 2014-2017 and was the Asian women's champion of 2018. Padmini Rout also played for the Indian national team at Women's Chess Olympiad, Women's World Team Chess Championship and Women's Asian Team Chess Championship. She won the individual gold medal while playing on the reserve board during the 2014 Women's Chess Olympiad in Tromsø, Norway and has been part of the Indian women's team in the subsequent Chess Olympiads in 2016 at Baku, Azerbaijan and 2018 at Batumi, Georgia."
+    ],
+    show_after: "2021-01-03T09:30:00+0000",
+    img_link: "padmini.jpg"
+  },
+  {
+    name: "Pranav Lal",
+    title: "Blind Photographer",
+    description: [
+      "Pranav Lal is a blind photographer. It almost sounds like an oxymoron, but he never lets his impairments stop him from trying things most of us would want to. Pranav suffered from Retinopathy of Prematurity at birth which resulted in him being completely blind ever since. However, this didn't stop the man from pursuing all that he was passionate about. Today, he is a cybersecurity professional who doubles up as a photographer. He is also an author of a blog 'Technesoterica' where he writes about technology related to sensory substitution.",
+      "In 2001, he stumbled upon an augment reality device called vOICe. The software essentially converts images into sound which he heares through the headphones and determines what he is looking at. He once wrote a passage in his blog revealing, 'My tool of photography is the vOICe which allows me to both frame the subject and to experientially get a feel of how light is falling on a subject and how something will look.' Beyond being a master of one trade, Pranav is a self-taught programmer and continues to program as a hobby. He also loves to travel and has journeyed exclusivley in India, including trekking to the Himalayas."
+    ],
+    show_after: "2021-01-04T09:30:00+0000",
+    img_link: "pranav.jpg"
+  },
+  {
+    name: "Viral Shah",
+    title: "CEO | Julia Computing",
+    description: [
+      "Rising in the technology industry with the belief that one must always 'go deep' when venturing into a new field, Viral Shah, an Indian computer scientist, is the co-founder and CEO of Julia Computing. Albeit a common belief that productivity and performance do not function in tandem in programming, he defied the odds through co-creation of the Julia Language, a high impact language that is now widely adopted all across the world.",
+      "Receiving his Ph.D. in the field of Combinatorial Scientific Computing from the University of California, Viral Shah strongly pursued his passing within the field as was seen through his active involvement in the Aadhaar Project, which provided Indian residents a 12-digit unique identity number based on their biometric and demographic data. Based on his vast experience and exposure within this industry and with regard to the government of India, he co-authored the Book Rebooting India; a call to reshape the country.",
+      "In 2019, he was awarded the J. H. Wilkinson Prize for Numerical Software for his work on the Julia Programming Language, and in 2013, he received The Spatial Ecology and Telemetry Working Group (SETWG) award for the co-creation of Circuitscape."
+    ],
+    show_after: "2021-01-04T09:30:00+000",
+    img_link: "viral.jpg"
+  }
 ]
-

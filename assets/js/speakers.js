0c4e38e1b599035457c52b481d8e7c86263fd953
Abhishek Kumar <abhishekkumar8222@gmail.com>
2021-02-10 18:25:42 +0530

    Add second batch of speakers

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -29,5 +29,48 @@ var speakers = [
     ],
     show_after: "2021-01-04T09:30:00+000",
     img_link: "viral.jpg"
+  },
+  {
+    name: "Smt. Minal Prabhu",
+    title: "Dancer | Mudrika Foundation",
+    description: [
+      "Smt. Minal Prabhu commenced her journey at Kalakshetra as a dancer, her training began for both Vocal Carnatic Music and Bharatanatyam; discovering her passion for the latter, she devoted hours while falling in love with the art form. Following her 10 years at Kalakshetra, she began stepping into the teacher's shoes, but never stopped her learning process. She fine-tuned her 'abhinaya' under Late Smt. Kalanidhi Narayanan, adopting elements of elegant storytelling into her own chereopgrahies.",
+      "In 1976, she founded the Mudrika Foundation for Indian Performing ARts to impart the Kalakshetra style of Bharatanatyam and is now one of the prominent dance gurus in India. She is an avid reader of mythological books as they form build the base for her productions and is also a beautiful medium to enhance her storytelling skills. She strongly believes in sharing her art as she goes beyond the walls of her foundation to teach Aesthetics at the Natya Institute of Kathak and Choreography, conduct Bharathanatyam classes at the Attakalari Center, conduct overseas workshops and even create digital traning material for students. This multifaceted artiste has coveted numerous awards including the Karnataka Kalashree award, the title of Bharak Kala chudar and so many more!"
+    ],
+    show_after: "2021-02-04T09:30:00+000",
+    img_link: "minal.jpg"
+  },
+  {
+    name: "Pallavi Khare & Meghna Chaudhury",
+    title: "Founders of WeUnlearn",
+    description: [
+      "Led by Pallavi Khare and Meghna Chaudhur, WeUnlearn impacts lives by building solutions across age groups to unlearn gender and mental health biases and discrimination. They are launching an AI chatbot Wulu, for adolescents to build their self-efficacy in dealing with gender and mental health issues. The chatbot uses a survey to categorize adolescents into low/medium/high risk (risk of being affected by gender bias and developing poor mental health).",
+      "They recently launched Call-it, a card game for children that uses evidence and research based gaming principles to help them overcome gender stereotypes.",
+      "WeUnlearn also has a strong online presence (community of ~27,000) where they empower youth to navigate the everyday world of bias and lead a gender-equitable life by breaking down complex social phenomena through research and facts into accessible bite sized chunks.",
+      "Pallavi has also worked in the Bill & Melinda Gates Foundation and is a graduate from Havard Kennedy School, IIT Delhi while Meghna has several entrepreneurial ventures in building sustainable and scalable solutions for gender equitability in India and globally.",
+      "Together, they plan to build a social enterprise in a more equitable world."
+    ],
+    show_after: "2021-02-04T09:30:00+000",
+    img_link: "pallavi_meghna.jpg"
+  },
+  {
+    name: "Raga D'Silva",
+    title: "LGBTQ Activist",
+    description: [
+      "She took the chance to shed her fears and live her truth. Finaly, Raga Olga D'Silva is a successful entrepreneur and Director of Speaking Minds. She has vast experience in the corporate sector and has worked in senior positions in advertising in India and New Zealand and has been recipient of several prestigious awards. Raga, chronicled aspects of this journey in her debut book, 'Untold Lies' and became one of India's most talked about authors in 2019. She chose to come out as a gay individual at a time when India had just decriminalized consensual homosexual conduct between adults.",
+      "Raga is in a strong position to discuss from her own experience the everyday issues that minorities and the LGBT community faces. In a society that has feared and ostracized the LGBT community, she speaks to instil empathy in those who look down on it and to comfort those who are a part of it. Her natural and relatable style makes her an engaging and relatable speaker who is capable of moving the audience. She continues to serve as an insiparation to those who aspire to their truest happiest self."
+    ],
+    show_after: "2021-02-04T09:30:00+000",
+    img_link: "raga.jpg"
+  },
+  {
+    name: "Pragya Prasun",
+    title: "Acid Attack Survivor | Activist",
+    description: [
+      "Pragya Prasun, a Bengaluru-based acid attack survivor turned activist and social worker has been fighting to provide relief and rehabilitation to her fellow acid attacks and burn survivors for the past eight years. Pragya had survived an acid attack by a jilted lover just 12 days after her marriage when she was 23. After her recovery, she started working to rehabilitate and empower acid attack survivors. She is the founder and secretary of 'Atijeevan Foundation', an NGO that apart from facilitating free-of-cost surgeries also provides avenues for income generation to several acid attacks and burn survivors by traning them to create handicraft products.",
+      "Pragya is a post-graduate in Fashion Management, and for her various exemplary work in field of Social Welfare, she is the recipient of several Indian and international awards and accolades. She received the Nari Shakti Puraskar awarded by the Government of India in recognition of her work. Pragya is also Ambassador and Mascot of Pinkathon, Ambassador of ASFI South India Chapter. She is also an Honorary Advisor to a few NGOs and social welfare communities. She says, 'Life will never go as planned. If I can change people's perception around me half the battle would be won, as beauty lies in the eye of a beholder.'"
+    ],
+    show_after: "2021-02-04T09:30:00+000",
+    img_link: "pragya.jpg"
   }
 ]

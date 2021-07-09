0f1f327b19e0a711cc8bca2599e7c153767ac914
Niraj Nandish <nirajnandish@icloud.com>
2021-07-09 17:18:39 +0400

    feat: optimised images

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -7,7 +7,7 @@ var speakers = [
       "As a professional in Big Data Analytics, his projects speak volumes about his genius, some projects even resulting in multi-million dollar impact! The ever-changing realities in AI and the exchange of data on the technology front have definitely brought both dangers and benefits for users lke us. Mr. Rao has been helping companies create significant values through analytics/AI and digital adoption - which makes him the perfect individual to help us think deeper than what is exposed to us."
     ],
     show_after: "2021-02-04T09:30:00+000",
-    img_link: "srinidhi.png"
+    img_link: "srinidhi.jpg"
   },
   {
     name: "Dr. Subi Chaturvedi",
@@ -17,7 +17,7 @@ var speakers = [
       "<b>Dr. Chaturvedi</b> has served as a member of the United Nations Internet Governance Forum (IGF) Multistakeholder Advisory Group (MAG), appointed by the UN Secretary-General. As a member, she has organised and the run the main session on Cybersecurity and Emerging issues at the United Nations IGF, throwing light on relvant trends and key issues in the sector. In the prestigious Women Achievers Summit & Awards 2021, Dr. Chaturvedi has been recognized as Innovative Leader of the year for leading the charge of shaping online gaming and the use of technology for good discourse in the public and policy space."
     ],
     show_after: "2021-02-04T09:30:00+000",
-    img_link: "subi.png"
+    img_link: "subi.jpg"
   },
   {
     name: "Raghunandan G",
@@ -27,6 +27,6 @@ var speakers = [
       "His time as an angel investor helped him understand various business models, sectors like SaaS, hyperlocal, AI and ML. Working in a startup that plans to go high scale early on, he recognizes what it is to work with extremely large amounts of data and its relevance to its operations. His approach towards the largely underserved global citizen community, entrepreneurship mindset and understanding of how data plays towards that, makes him an excellent speaker to provide insight into opportunities untapped and the role of data in the future."
     ],
     show_after: "2021-02-04T09:30:00+000",
-    img_link: "raghu.png"
+    img_link: "raghu.jpg"
   }
 ]

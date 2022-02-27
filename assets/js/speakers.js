39dca59486ab11c558ffe87068e264e15b12c783
Niraj Nandish <nirajnandish@icloud.com>
2022-02-27 09:00:05 +0400

    feat: Add Feb salon speakers

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,33 +1,21 @@
 var speakers = [
   {
-    name: "Karan Shah",
-    title: "Founder and CEO of IIDE | Edu-preneur",
+    name: "Aryamba Sriram",
+    title: "Bharatnatiyam Dancer",
     description: [
-      "<b>Karan Shah</b>, a tech visionary, educationist, public speaker, and the Founder of IIDE is indeed a multi-faceted personality. After specializing in E-commerce Strategies from Harvard University, Mr. Karan returned to India as he identified a digital skill gap and set up IIDE, India’s premier Ed-tech School for Digital Skills to harness the opportunities in education and work that came along in the digital age. His firm has trained leading corporations like Mahindra, Volkswagen, Asian Paints, Pfizer, Estee Lauder, and World Bank in Digital Skills. The World Education Congress awarded IIDE as the ‘Best Digital Marketing Institute of 2021.'",
-      "From teaching 8 students out of his grandfather's garage to a 20,000 sq feet campus and now, moving online and helping 2,35,000+ students and professionals, Mr. Karan has helped a lot of individuals become employable and entrepreneurial. He is also a visiting faculty at the IIMs and 14+ Mumbai University colleges. Mr. Karan has over a decade of experience in the field of digital marketing and is the perfect speaker to provide a deeper insight into social media and content creation."
+      "<b>Aryamba Sriram</b> is an accomplished Bharathanatyam dancer, having received advanced training in Kalakshetra Foundation. She has been part of several international dance ensembles and has extensively performed globally, representing India. To her credit are several awards and recognitions.",
+      "Her conviction of art having scientific roots and reasoning has driven her to pursue a Master's degree in Vedic Science. A talented and curious speaker, Aryamba Sriram promises to provide a curiosity-driven outlook to the beautiful world of classical dance."
     ],
     show_after: "2021-02-04T09:30:00+000",
-    img_link: "karan.jpg"
+    img_link: "aryamba.png"
   },
   {
-    name: "Sarah Donawerth",
-    title: "Web Content Coordinator | Freelance Artist",
+    name: "Nalini Ramachandran",
+    title: "Writer & Editor",
     description: [
-      "<b>Sarah Donawerth</b> is a freelance artist whose passion for art is apparent in the various aspects she explores which include knitting, crochet, jewelry making and paper crafts. Her work has been featured in some of the top art and craft magazines including Bella Grace, Jewelry Affaire, Belle Armoire Jewelry, and more.",
-      "Apart from more traditional forms of fine art, Sarah has great experience in the field of digital media marketing and content writing as a freelance blogger. Her experience in several firms and start-ups in different job roles serves to diversify her experience and wealth of knowledge. Uniting her love for art with marketing skills, she has worked as a Social Media Manager & Designer at Stampington & Co, the largest publisher of niche magazines in the art & craft industry. Her previous roles as a Marketing Manager and Content Manager lend her insight into how effective marketing performs on social platforms.",
-      "As someone who understands both, the perspective of an artist seeking an audience on social media as well as the mindset of marketing companies, she is sure to have a well-rounded understanding of the two-sided coin that is the ‘Attention trap’."
+      "In the past two decades, <b>Nalini Ramachandran</b> has worked as a writer and editor across various media—film and television, newspapers and magazines, textbooks and coffee-table books, children's books and graphic novels. She takes a keen interest in Indian culture, especially the art and craft of storytelling. She is the author of Lore of the Land: Storytelling Traditions of India; Nava Durga: The Nine Forms of the Goddess; APJ Abdul Kalam: One Man, Many Missions; Gods, Giants and the Geography of India; Trumpet Calls: Epic Tales of Extraordinary Elephants."
     ],
     show_after: "2021-02-04T09:30:00+000",
-    img_link: "sarah.jpg"
+    img_link: "nalini.png"
   },
-  {
-    name: "Ankur Grover",
-    title: "Co-Founder of Tinkerlabs | Design Thinker",
-    description: [
-      "A man of remarkable talents and a mind of an intricate design thinker, <b>Ankur Grover</b> is definitely a name to reckon with. He is the co-founder of TinkerLabs, India’s only design thinking firm, that allows individuals and organizations to harness this domain to empathize, collaborate and experiment, thus producing agile and effective solutions for any available niche. Apart from being an accomplished entrepreneur, Ankur is a seasoned TEDx speaker who sheds further light on the intriguing aspects of design thinking.",
-      "Being an alumnus of BITS Pilani and INSEAD, he has a vast amount of experience in design engineering, management and anthropology. A skillful individual and a winner of the two most prestigious scholarships offered by INSEAD, Ankur has showcased his expertise in this field of study. He is also a specialist in areas of network engineering and business development, where he co-authored a Harvard Review Case Study titled ‘Innovation and Design Thinking’. Through Ankur’s unique storytelling expertise, he has ensured that every organization recognizes the crucial need to adopt design thinking."
-    ],
-    show_after: "2021-02-04T09:30:00+000",
-    img_link: "ankur.jpg"
-  }
 ]

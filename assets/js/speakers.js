144427dbe5d611dfff5437a0a0a3b8dbbca77966
Niraj Nandish <nirajnandish@icloud.com>
2022-03-23 09:44:08 +0530

    update time and released speakers

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,21 +1,80 @@
 var speakers = [
   {
-    name: "Aryamba Sriram",
-    title: "Bharatnatiyam Dancer",
+    name: 'Anil Pradhan',
+    title: 'Founder & CEO | Young Tinker Academy',
     description: [
-      "<b>Aryamba Sriram</b> is an accomplished Bharathanatyam dancer, having received advanced training in Kalakshetra Foundation. She has been part of several international dance ensembles and has extensively performed globally, representing India. To her credit are several awards and recognitions.",
-      "Her conviction of art having scientific roots and reasoning has driven her to pursue a Master's degree in Vedic Science. A talented and curious speaker, Aryamba Sriram promises to provide a curiosity-driven outlook to the beautiful world of classical dance."
+      '<b>Anil Pradhan</b> is an Indian engineer, an innovator and a revolutionary educationist. He is the founder of Young Tinker and School for Rural Innovation (IPSFRI), which was founded in 2015 to educate the less-privileged students of Odisha through an interdisciplinary curriculum. Starting with three students, his team is now impacting the lives of 1,50,000+ students across India. He is well known for designing, innovative labs, teaching tools, indigenous rockets and projects which solves real-life problems.',
+      "In 2020, he started Young Tinker Academy, World's 1st Live online hands-on learning academy. This Ed-Tech start-up from India is now going global with its students across the Globe. This Ed-Tech platform created a U-19 Indian team that secured World Rank 3 in NASA's Rover Challenge 2021 and made India proud by becoming Ist U-19 Asian team to do so.",
+      'This year, he received the National Youth Award from the Govt of India for his contribution towards Education, Science and technology.',
     ],
-    show_after: "2021-02-04T09:30:00+000",
-    img_link: "aryamba.png"
+    show_after: '2021-02-04T09:30:00+000',
+    img_link: 'anil.png',
   },
   {
-    name: "Nalini Ramachandran",
-    title: "Writer & Editor",
+    name: 'Susan Thomas',
+    title: 'Director | NIFT Bengaluru',
     description: [
-      "In the past two decades, <b>Nalini Ramachandran</b> has worked as a writer and editor across various media—film and television, newspapers and magazines, textbooks and coffee-table books, children's books and graphic novels. She takes a keen interest in Indian culture, especially the art and craft of storytelling. She is the author of Lore of the Land: Storytelling Traditions of India; Nava Durga: The Nine Forms of the Goddess; APJ Abdul Kalam: One Man, Many Missions; Gods, Giants and the Geography of India; Trumpet Calls: Epic Tales of Extraordinary Elephants."
+      "<b>Susan Thomas</b>, IRS, is a public speaker, debater, Director of the National Institute of Fashion Technology, Bengaluru, and all-around a woman of many accomplishments. She is an alumna of prestigious universities namely, St Teresa's, JNU, the University of Oxford (Masters in Public Policy). She has worked with the Income Tax Department for 17 years, including many years in remote places in Rajasthan, as well as the Ministry of Textiles. She has spoken at TEDx before at TEDxChoiceSchool and TEDxGITAMVizag. On social media, she speaks about NIFT Bengaluru, textiles and culture, being a government employee, and feminism.",
     ],
-    show_after: "2021-02-04T09:30:00+000",
-    img_link: "nalini.png"
+    show_after: '2021-02-04T09:30:00+000',
+    img_link: 'susan.png',
   },
-]
+  {
+    name: 'Vahbiz Bharucha',
+    title: "Captain | Indian Women's Rugby Team",
+    description: [
+      '<b>Vahbiz Bharucha</b> is the captain of the Indian Women Rugby Team. A seasoned TEDx speaker, physiotherapist and medical educator, Vahbiz is a name to reckon with. Being a rugby player and having a career in physiotherapy, she depicts how one can balance between two different passions in their life. She has been more than instrumental in developing the craze for the sport of rugby amongst the general population and has played a crucial role in encouraging more women to take up the sport.',
+      "Vahbiz Bharucha is often regarded as 'India's Wonder Woman' by several articles and blogs. Her commitment, dedication and admiration for the sport have been clearly reflected through her work habits and professional life. From kick-off to a perfect touchdown, Vahbiz's journey as an athlete has been inspirational and a barrier-breaker for all the people out there!",
+    ],
+    show_after: '2021-02-04T09:30:00+000',
+    img_link: 'vahbiz.png',
+  },
+  {
+    name: 'Neha Cadabam',
+    title: 'Consultant Psychologist',
+    description: [
+      "<b>Neha Cadabam</b> is a Psychologist at Cadabam's Hospitals and an Executive Director at the Cadabam's Group. She's a Consultant Psychologist and is also a certified Neuro-Linguistic Practitioner with certification from Dr. Richard Brander, one of the pioneers of the Neurolinguistic Programming approach to psychology. With over 1l years of experience in the field of mental health, her area of interest is preventive and promotive mental healthcare. She is very passionate about what she does and works closely in helping individuals to improve their mental health and in promoting lifestyle changes, healthier habits in daily life.",
+      'She specializes in treating various mental health issues like stress management, depression, anxiety, and many others. She has a fond interest in working with children too. Apart from her skills in psychology, she has expertise in administrative and management of care giving facilities.',
+    ],
+    show_after: '2021-02-04T09:30:00+000',
+    img_link: 'neha.png',
+  },
+  {
+    name: 'Ankush Jain',
+    title: 'Multi-Instrumental Beatboxer',
+    description: [
+      "A musician, book enthusiast, foodie, nature enthusiast, stage performer, and most importantly, India's first multi-instrumental beat-boxer, Ankush Jain is a jack of many trades. Having performed in over 200 shows, he is now looking forward to heal the world with his music therapy. He describes himself to be a curiosity-driven person.",
+      "Being born into a family of businessmen, Ankush Jain has a bachelor's degree in commerce. But his love for music never went away. He has performed along with Armaan Malik and other leading artists from the Indian music community. He hopes to venture into music therapy, which has now become an instrumental remedy for individuals with ADHD, Down syndrome, among others.",
+      'Having already performed in a TEDx event before, in TEDXPCCOE Pune, Ankush Jain is excited to be on the TEDx stage and own it again.',
+    ],
+    show_after: '2021-02-04T09:30:00+000',
+    img_link: 'ankush.png',
+  },
+  {
+    name: 'Susan Thomas',
+    title: 'Director | NIFT Bengaluru',
+    description: [
+      "<b>Susan Thomas</b>, IRS, is a public speaker, debater, Director of the National Institute of Fashion Technology, Bengaluru, and all-around a woman of many accomplishments. She is an alumna of prestigious universities namely, St Teresa's, JNU, the University of Oxford (Masters in Public Policy). She has worked with the Income Tax Department for 17 years, including many years in remote places in Rajasthan, as well as the Ministry of Textiles. She has spoken at TEDx before at TEDxChoiceSchool and TEDxGITAMVizag. On social media, she speaks about NIFT Bengaluru, textiles and culture, being a government employee, and feminism.",
+    ],
+    show_after: '2022-03-24T16:00:00.000+05:30',
+    img_link: 'susan.png',
+  },
+  {
+    name: 'Susan Thomas',
+    title: 'Director | NIFT Bengaluru',
+    description: [
+      "<b>Susan Thomas</b>, IRS, is a public speaker, debater, Director of the National Institute of Fashion Technology, Bengaluru, and all-around a woman of many accomplishments. She is an alumna of prestigious universities namely, St Teresa's, JNU, the University of Oxford (Masters in Public Policy). She has worked with the Income Tax Department for 17 years, including many years in remote places in Rajasthan, as well as the Ministry of Textiles. She has spoken at TEDx before at TEDxChoiceSchool and TEDxGITAMVizag. On social media, she speaks about NIFT Bengaluru, textiles and culture, being a government employee, and feminism.",
+    ],
+    show_after: '2022-03-24T16:00:00.000+05:30',
+    img_link: 'susan.png',
+  },
+  {
+    name: 'Susan Thomas',
+    title: 'Director | NIFT Bengaluru',
+    description: [
+      "<b>Susan Thomas</b>, IRS, is a public speaker, debater, Director of the National Institute of Fashion Technology, Bengaluru, and all-around a woman of many accomplishments. She is an alumna of prestigious universities namely, St Teresa's, JNU, the University of Oxford (Masters in Public Policy). She has worked with the Income Tax Department for 17 years, including many years in remote places in Rajasthan, as well as the Ministry of Textiles. She has spoken at TEDx before at TEDxChoiceSchool and TEDxGITAMVizag. On social media, she speaks about NIFT Bengaluru, textiles and culture, being a government employee, and feminism.",
+    ],
+    show_after: '2022-03-24T16:00:00.000+05:30',
+    img_link: 'susan.png',
+  },
+];

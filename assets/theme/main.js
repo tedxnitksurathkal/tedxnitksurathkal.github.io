66708fa371757d131c5dd3d282ef6c3e09a17f3f
Niraj Nandish <nirajnandish@icloud.com>
2022-10-06 23:18:39 +0400

    remove previosu theme code

--- assets/theme/main.js
+++
@@ -1,7 +0,0 @@
-import { ProjectComponent } from './components/project/project.cmp.js';
-
-let appContainer;
-const page = new ProjectComponent();
-appContainer = document.body.querySelector('.app');
-appContainer.innerHTML = '';
-appContainer.append(page);

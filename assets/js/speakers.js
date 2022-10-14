ecde6449f47856a3b2dd7b5afef1f3631be2854f
Niraj Nandish <nirajnandish@icloud.com>
2022-10-14 10:59:41 +0530

    feat: october 2022 salon speakers

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,84 +1,33 @@
 var speakers = [
   {
-    name: 'Anil Pradhan',
-    title: 'Founder & CEO | Young Tinker Academy',
+    name: 'Amit Nigam',
+    title: 'Vice President of Products | RedBus',
     description: [
-      '<b>Anil Pradhan</b> is an Indian engineer, an innovator and a revolutionary educationist. He is the founder of Young Tinker and School for Rural Innovation (IPSFRI), which was founded in 2015 to educate the less-privileged students of Odisha through an interdisciplinary curriculum. Starting with three students, his team is now impacting the lives of 1,50,000+ students across India. He is well known for designing, innovative labs, teaching tools, indigenous rockets and projects which solves real-life problems.',
-      "In 2020, he started Young Tinker Academy, World's 1st Live online hands-on learning academy. This Ed-Tech start-up from India is now going global with its students across the Globe. This Ed-Tech platform created a U-19 Indian team that secured World Rank 3 in NASA's Rover Challenge 2021 and made India proud by becoming Ist U-19 Asian team to do so.",
-      'This year, he received the National Youth Award from the Govt of India for his contribution towards Education, Science and technology.',
+      '<b>Amit Nigam</b> is an hands-on product leader, entrepreneur, and a passionate educator/speaker on a wide range of product management topics. He comes with over 2 decades of experience in the areas of B2C e-commerce and B2B Saas. In his last entrepreneurial stint, Amit founded an internet startup leveraging Al to create a CRM system for automotive aftermarket, before exiting the business through a successful acquisition by the TVS group. Amit holds 4 technology patents and is a visiting faculty of product management at multiple Indian institutes of eminence.',
     ],
     show_after: '2021-02-04T09:30:00+000',
-    img_link: 'anil.png',
+    img_link: 'amit.png',
   },
   {
-    name: 'Susan Thomas',
-    title: 'Director | NIFT Bengaluru',
+    name: 'Pratyush Prabhakar & Divya Singh',
+    title: 'Public Policy Professional | Communeeti',
     description: [
-      "<b>Susan Thomas</b>, IRS, is a public speaker, debater, Director of the National Institute of Fashion Technology, Bengaluru, and all-around a woman of many accomplishments. She is an alumna of prestigious universities namely, St Teresa's, JNU, the University of Oxford (Masters in Public Policy). She has worked with the Income Tax Department for 17 years, including many years in remote places in Rajasthan, as well as the Ministry of Textiles. She has spoken at TEDx before at TEDxChoiceSchool and TEDxGITAMVizag. On social media, she speaks about NIFT Bengaluru, textiles and culture, being a government employee, and feminism.",
+      '<b>Pratyush Prabhakar</b> is a public policy professional. A tech graduate who landed up in a B-school before foraying into public policy. Pratyush believes in bringing together the best of humanities, business and technology to achieve larger good in the society. He is an alumnus of IIM Calcutta and NIT Nagpur.',
+      'Pratyush, along with Divya Singh, wrote an article that was published in The Hindu on "The age of hyperlapse consumerism." The article analyzes the costs and benefits of fast deliveries, changing nature of consumerism, analyzing it from a social, philosophical and behavioral perspective and the need for consumers and businesses to be mindful of the consequences of their actions.',
+      '<b>Divya Singh Rathore</b> is a public policy professional. A history buff, a culture enthusiast and a trained classical dancer, Divya likes to analyze contemporary trends with a historical and cultural lens. She is a graduate from Lady Sri Ram College and Postgraduate from Hindu College, Delhi University. She is a travel enthusiast who loves interacting with people from different cultures to understand the grassroot issues which can be resolved through governance and public policy.',
+      'Divya, along with Pratyush Prabhakar, wrote an article that was published in The Hindu on "The age of hyperlapse consumerism." The article analyzes the costs and benefits of fast deliveries, changing nature of consumerism, analyzing it from a social, philosophical and behavioral perspective and the need for consumers and businesses to be mindful of the consequences of their actions.'
     ],
     show_after: '2021-02-04T09:30:00+000',
-    img_link: 'susan.png',
+    img_link: 'pratyush_divya.png',
   },
   {
-    name: 'Vahbiz Bharucha',
-    title: "Captain | Indian Women's Rugby Team",
+    name: 'Arjun DSouza',
+    title: "Director | Alter Marketing Solutions",
     description: [
-      '<b>Vahbiz Bharucha</b> is the captain of the Indian Women Rugby Team. A seasoned TEDx speaker, physiotherapist and medical educator, Vahbiz is a name to reckon with. Being a rugby player and having a career in physiotherapy, she depicts how one can balance between two different passions in their life. She has been more than instrumental in developing the craze for the sport of rugby amongst the general population and has played a crucial role in encouraging more women to take up the sport.',
-      "Vahbiz Bharucha is often regarded as 'India's Wonder Woman' by several articles and blogs. Her commitment, dedication and admiration for the sport have been clearly reflected through her work habits and professional life. From kick-off to a perfect touchdown, Vahbiz's journey as an athlete has been inspirational and a barrier-breaker for all the people out there!",
+      '<b>Mr. Arjun John DSouza</b> is an entrepreneur and the Co-founder/Managing Director of Alter Marketing Solutions Pvt. Ltd., a creative digital marketing agency based in Mangalore, India. Alter offers complete marketing solutions to its clients which include but are not limited to multi-channel digital marketing, consulting, branding, graphic design, web development, photo, and video ad production.',,
+      'A former sailor in the merchant navy, Mr. Souza believes that his passion for marketing began while sailing across various countries and meeting people from all walks of life. He started Alter with a vision of creating a world where every good idea has a chance. He believes that marketing, especially digital marketing, can work for anyone and everyone if executed right. In the last 5 years, he has built Alter from the ground up, starting with a small team of 4 to now 50, by servicing multiple local and multi-national brands. Additionally, he has and continues to conduct technical workshops and seminars on marketing, digital marketing and its various possibilities.'
     ],
     show_after: '2021-02-04T09:30:00+000',
-    img_link: 'vahbiz.png',
-  },
-  {
-    name: 'Neha Cadabam',
-    title: 'Consultant Psychologist',
-    description: [
-      "<b>Neha Cadabam</b> is a Psychologist at Cadabam's Hospitals and an Executive Director at the Cadabam's Group. She's a Consultant Psychologist and is also a certified Neuro-Linguistic Practitioner with certification from Dr. Richard Brander, one of the pioneers of the Neurolinguistic Programming approach to psychology. With over 1l years of experience in the field of mental health, her area of interest is preventive and promotive mental healthcare. She is very passionate about what she does and works closely in helping individuals to improve their mental health and in promoting lifestyle changes, healthier habits in daily life.",
-      'She specializes in treating various mental health issues like stress management, depression, anxiety, and many others. She has a fond interest in working with children too. Apart from her skills in psychology, she has expertise in administrative and management of care giving facilities.',
-    ],
-    show_after: '2021-02-04T09:30:00+000',
-    img_link: 'neha.png',
-  },
-  {
-    name: 'Ankush Jain',
-    title: 'Multi-Instrumental Beatboxer',
-    description: [
-      "A musician, book enthusiast, foodie, nature enthusiast, stage performer, and most importantly, India's first multi-instrumental beat-boxer, Ankush Jain is a jack of many trades. Having performed in over 200 shows, he is now looking forward to heal the world with his music therapy. He describes himself to be a curiosity-driven person.",
-      "Being born into a family of businessmen, Ankush Jain has a bachelor's degree in commerce. But his love for music never went away. He has performed along with Armaan Malik and other leading artists from the Indian music community. He hopes to venture into music therapy, which has now become an instrumental remedy for individuals with ADHD, Down syndrome, among others.",
-      'Having already performed in a TEDx event before, in TEDXPCCOE Pune, Ankush Jain is excited to be on the TEDx stage and own it again.',
-    ],
-    show_after: '2021-02-04T09:30:00+000',
-    img_link: 'ankush.png',
-  },
-  {
-    name: 'Saad Khan',
-    title: 'Director, Screenwriter and Improv Comedian',
-    description: [
-      'An Indian director, screenwriter and acting teacher and Improv comedian, <b>Saad Khan</b> is an emerging young talent. He is also the founder of FirstAction Studios and Centre Stage, a new wave media movement based in Bangalore that promotes new artists in the city, he has shown his passion for the film industry.',
-      "Khan is an alumnus of St. Joseph's College, eventually graduating as a mechanical engineer from M.S. Ramaiah Institute of Technology. While studying to be an engineer, he developed an interest in theatre at the age of 17 and started writing and directing plays, also spearheading a theatre group which then produced over 12 plays in a span of 4 years. Saad then decided to pursue a career in film and media and after graduating from college, he left for the United States to continue his education in film-making.",
-      "He is known for his Kannada-English feature film Humble Politician Nograj(2018), which became a hit and the first Kannada film to achieve world-wide digital audience presence on Amazon's Prime Video plattorm. After working in Bollywood as an Associate Director, Saad made Bangalore's first Hindi feature film Station, independently produced by Sumit Ghosh and released across India and Indonesia. In 2015, Saad filmed his first Hollywood feature film titled 'Love and She' in Louisiana.",
-    ],
-    show_after: '2021-02-04T09:30:00+000',
-    img_link: 'saad.png',
-  },
-  {
-    name: 'Suhas Gopinath',
-    title: 'Founder and CEO | Globals INC',
-    description: [
-      'Young and dynamic, <b>Suhas Gopinath</b> is a name to reckon with in this digital start up era. Recognized by the European parliament and International association for Human Values as "Young Achiever Award", he is one of the youngest members in the World Economic Forum. Coming from a very humble background, he was a student who was very interested in programming culture of computer science. At the time when computer just made its first appearance, he bartered a deal with an internet café owner in his neighbourhood, where he used to take care of the shop during lunch hours and get free access to the computer he always dreamt of. He listed himself as a freelancer, and started his own website by the name of coolhindustan.com that catered to the NRIs. An experienced entrepreneur with a demonstrated history of working in EdTech, FinTech, SaaS, he has engagements with multilateral and government bodies. He is the co founder and CEO of HappyEMl and Globals Inc., a multinational IT company which he took charge as CEO at the age of 17, merely three years after founding the company.',
-    ],
-    show_after: '2021-02-04T09:30:00+000',
-    img_link: 'suhas.png',
-  },
-  {
-    name: 'Kavan Kuttappa',
-    title: 'Founder | Naru Noodle Bar',
-    description: [
-      "<b>Chef Kavan Kuttappa</b> is the founder of Eatnaru and former Culinary Head of The Permit Room and Toit, Bengaluru. He is an engineer turned chef who is transforming India's culinary landscape. After completing his engineering he realised his true calling was to follow his love for food and decided to get a culinary degree from CIA (Culinary Institute of America). He has had stints at prestigious establishments like Eleven Madison Park (voted the World's Best Restaurant in 2017), Le Bernardin, Jean Georges, and Momofuku in New York.",
-      "During his stint as Culinary Head at The Permit Room, he disrupted and reimagined bar food by serving South Indian grub at a pub that became a cult favorite in no time. In 2020, he was featured in 'Condé Nast Traveller's 40 under 40: India's most exciting young chefs'.",
-      "After working in the food and beverage industry for a decade, Chef Kavan decided to work on his passion project and launched his venture - 'Naru Noodle Bar', where he sends Ramen kits to Bangaloreans on a weekly basis. It has been the talk of the town since its inception and has been featured on Vogue India, Deccan Herald, The Hindu Weekend, ELLE India and other prominent media houses.",
-    ],
-    show_after: '2021-02-04T09:30:00+000',
-    img_link: 'kavan.png',
+    img_link: 'arjun.png',
   },
 ];

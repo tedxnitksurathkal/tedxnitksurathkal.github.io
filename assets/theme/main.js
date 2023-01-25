b8c75c9bc9637900a09e63962fdf8ef3b9ac5bf0
Niraj Nandish <nirajnandish@icloud.com>
2023-01-25 20:50:03 +0530

    feat: february 2023 main event theme reveal (#144)
    * feat: first round
    
    * feat: remaining rounds images
    
    * feat: finish up reveal
    
    * fix: update site title
    
    * feat: negative points for wrong answer
    
    * feat: content and image

---
+++ assets/theme/main.js
@@ -0,0 +1,40 @@
+export const CODE = 'MOSAIC';
+
+export const IMAGEMAP = {
+  M: {
+    1: 'Mouse',
+    2: 'Milk',
+    3: 'Money',
+    4: 'Mango',
+  },
+  O: {
+    1: 'Orange',
+    2: 'Onion',
+    3: 'Octopus',
+    4: 'Owl',
+  },
+  S: {
+    1: 'Snail',
+    2: 'Snake',
+    3: 'Ship',
+    4: 'Soap',
+  },
+  A: {
+    1: 'Apple',
+    2: 'Ant',
+    3: 'Airplane',
+    4: 'Ambulance',
+  },
+  I: {
+    1: 'Ice-cream',
+    2: 'Ink',
+    3: 'Ice',
+    4: 'Iron',
+  },
+  C: {
+    1: 'Cherry',
+    2: 'Cake',
+    3: 'Carrot',
+    4: 'Cheese',
+  },
+};

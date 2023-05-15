020d3a7bb5481166eeecc3deceb51ed95abd1367
violinadoley <97302655+violinadoley@users.noreply.github.com>
2023-05-15 18:24:06 +0530

    Update team-list.js

--- assets/js/team-list.js
+++ assets/js/team-list.js
@@ -38,84 +38,84 @@ var team = [
         "name": "Ranjana Kambhammettu",
         "position": "Team Lead",
         "image": "Ranjana.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/ranjanakambhammettu/",
         "instagram": ""
     },
     {
         "name": "Advaith Curpod",
         "position": "Advisor",
         "image": "Advaith.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/advaith-curpod/",
         "instagram": ""
     },
     {
         "name": "Devvrat Ashtaputre",
         "position": "Advisor",
         "image": "Devvrat.jpeg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/devvrat-ashtaputre-839a13201/",
         "instagram": ""
     },
     {
         "name": "Vibhu Varshney",
         "position": "Head Of Curation",
         "image": "Vibhu.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/vibhuvarshney081103/",
         "instagram": ""
     },
     {
         "name": "Vartika Rao",
         "position": "Head Of Curation",
         "image": "Vartika.jpg",
-        "linkedin": "https://www.linkedin.com/in/ranjanakambhammettu/",
-        "instagram": "ranjana_kambhammettu"
+        "linkedin": "https://www.linkedin.com/in/vartika-t-rao/",
+        "instagram": ""
     },
     {
         "name": "Punith Raj",
         "position": "Head Of Production",
         "image": "Punith.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/punith-raj-836926209/",
         "instagram": ""
     },
     {
         "name": "Rohit Chabbi",
         "position": "Head Of Production",
         "image": "Rohit.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/rohit-chabbi/",
         "instagram": ""
     },
     {
         "name": "Abhishek P",
         "position": "Head Of Operations",
         "image": "Abhishek.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/abhishek-p-285b05218/",
         "instagram": ""
     },
     {
         "name": "Pulkit Jain",
         "position": "Head Of Operations",
         "image": "Pulkit.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/pulkitjain0151/",
         "instagram": ""
     },
     {
         "name": "Hemant Somanna",
         "position": "Head Of Marketing",
         "image": "Hemant .jpg",
-        "linkedin": "https://www.linkedin.com/in/devvrat-ashtaputre-839a13201        ",
+        "linkedin": "https://www.linkedin.com/in/hemanth-somanna-b53310208/",
         "instagram": ""
     },
     {
         "name": "Violina Doley",
         "position": "Web Manager",
         "image": "Violina.jpg",
-        "linkedin": "https://www.linkedin.com/in/niraj-n-656761191/",
+        "linkedin": "https://www.linkedin.com/in/violinadoley/",
         "instagram": ""
     },
     {
         "name": "Poorvi Joshi",
         "position": "Creative Head",
         "image": "Poorvi.jpg",
-        "linkedin": "",
+        "linkedin": "https://www.linkedin.com/in/poorvijoshi/",
         "instagram": ""
     },
 ]

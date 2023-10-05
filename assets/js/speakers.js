5b4c9dbd7d4f3258e66fa95f1e0c853331aab1d5
violinadoley <97302655+violinadoley@users.noreply.github.com>
2023-10-05 19:54:19 +0530

    Update speakers.js

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -8,7 +8,7 @@ var speakers = [
       'As a trainer and motivational speaker, he has influenced over a thousand individuals and aided organizations in achieving synergy among people, resources, and technology. Dr. Topaz Dharmaraj’s notable achievements include organizing the "Freedom Climb" to commemorate India’s independence. He has been honored with the "National Integration Award for Peace" from the Government of Karnataka.',
       'His eloquence as an orator is evident through numerous guest speaking engagements at prestigious institutions and events, making him a prominent figure in the field of psychology and human behavior.',
     ],
-    show_after: '2021-02-04T09:30:00+000',
+    show_after: '2023-10-04T09:30:00+000',
     img_link: 'dhar.jpg',
   },
   {
@@ -19,7 +19,7 @@ var speakers = [
       'Mrs. Swetha brings a wealth of knowledge and experience to her role, having also served as a clinical psychologist at Manasa Nursing Home. Her impressive skills span a wide spectrum of psychological disciplines, including Mental Health, Psychotherapy, Psychology, Psychological Assessment, Group Therapy, Counseling Psychology, Clinical Supervision, and Mental Health Counseling.',
       'In a world where mental health is of utmost importance, Mrs. Swetha KT stands as a beacon of hope and support for those seeking guidance and healing. Her dedication to her craft and her commitment to improving the lives of others make her an invaluable asset to the field of psychology. Through her work, she helps individuals navigate the complexities of their minds and find the path to mental and emotional well-being. Swetha KT is a true advocate for mental health, and her contributions are a testament to her passion for making a positive difference in the lives of those she serves.',
     ],
-    show_after: '2021-02-04T09:30:00+000',
+    show_after: '2023-10-04T09:30:00+000',
     img_link: 'swe.jpg',
   },
   {
@@ -30,7 +30,7 @@ var speakers = [
       "Mrs. Vinatha is a published author in addition to being a teacher. Her scholarly work has received praise from the academic community and is a testament to her dedication to advancing our understanding of human nature. Her book is a guiding light for knowledge, illuminating the complexities that make us wholly human. Mrs. Vinatha K. is an accomplished speaker in addition to being a prolific writer. She has spoken on numerous stages, captivating audiences with her speeches on leadership and human behavior. Her messages strike a deep chord with her listeners, igniting discussions that result in transformation and personal growth.",
       'She has the charisma of a storyteller and the wisdom of a sage, and is bound to whisk us away on a journey through the fascinating realms of psychology and leadership through her captivating talk.',
     ],
-    show_after: '2021-02-04T09:30:00+000',
+    show_after: '2023-10-04T09:30:00+000',
     img_link: 'vina.jpg',
   },
   

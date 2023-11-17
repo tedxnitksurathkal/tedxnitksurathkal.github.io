8e68fe11ac89ed186d2e533df6a2f4ac454fa8e2
Violina Doley <97302655+violinadoley@users.noreply.github.com>
2023-11-17 13:49:48 +0530

    Update speakers.js

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,37 +1,31 @@
 var speakers = [
   
   {
-    name: 'Dr. Dharmaraj Topaz',
-    title: 'Neuro-Psychologist',
+    name: 'Sairaj Dhond',
+    title: 'Founder & CEO - Wakao Foods',
     description: [
-      '<b>Dr. Dharmaraj Topaz</b>, is a distinguished scholar and researcher, honored with a doctoral scholarship by the Trinity College Research Foundation (UK) for his work on language and human behavior. With over 35 years of experience as a neuro-psychologist, he specializes in educational and corporate psychology, focusing on sustainable performance and growth.',
-      'As a trainer and motivational speaker, he has influenced over a thousand individuals and aided organizations in achieving synergy among people, resources, and technology. Dr. Topaz Dharmaraj’s notable achievements include organizing the "Freedom Climb" to commemorate India’s independence. He has been honored with the "National Integration Award for Peace" from the Government of Karnataka.',
-      'His eloquence as an orator is evident through numerous guest speaking engagements at prestigious institutions and events, making him a prominent figure in the field of psychology and human behavior.',
+      '<b>Mr. Wakao</b>, as he is famously known, Sairaj Dhond is the founder and CEO of Wakao Foods, a jackfruit-based, vegan Food Company. A lawyer by education and an entrepreneur by passion, Sairaj began his career as a criminal lawyer but soon defied his academic-led profession in pursuit of his true calling, sustainable entrepreneurship. Hailing from a lineage of business experts, Sairaj had a natural inclination towards entrepreneurship from a young age. A serial entrepreneur at heart, Sairaj is deeply committed to creating brands that create a global footprint and help the environment. His company, Wakao Foods has been recognised as India first and Most Loved Plant-Based Product Company. 2022 came in as a major turning point in the life of Brand Wakao, as it became India first start-up to be backed by all three female sharks on Shark Tank India. The brand received funding from SUGAR Cosmetics, Mama Earth, and Emcure Pharmaceuticals in the 1st edition of Shark Tank India that aired on Sony Entertainment Television. Sairaj is also a successful serial entrepreneur, and his other business ventures include a lighting and solar solutions business, and a real estate restoration business called Luxo Heritage Homes. Sairaj is also associated with social organizations like the Indian Red Cross Society, Goa Branch, The CII Young Indians, The Rotary Movement, and The Jaycees. Sairaj has been awarded the Business World Young Entrepreneur Award in 2021. When not working, Sairaj can be found playing cricket, and he is a state-level cricket champion, who thoroughly enjoys playing and following the game to this day.',
     ],
     show_after: '2023-10-04T09:30:00+000',
-    img_link: 'dhar.jpg',
+    img_link: 'Sairaj.jpg',
   },
   {
-    name: 'Mrs. Swetha KT',
-    title: 'Clinical Psychologist',
+    name: 'Puneeth Suraana',
+    title: 'Audio Producer',
     description: [
-      '<b>Mrs. Swetha KT</b>, is a dedicated professional in the field of mental health and psychology, making a profound impact on the well-being of individuals. With her unwavering commitment and expertise, she serves as the Director and Psychologist at the Anirvedha Resource Centre for Psychological Well-Being, where her passion for helping others shines brightly.',
-      'Mrs. Swetha brings a wealth of knowledge and experience to her role, having also served as a clinical psychologist at Manasa Nursing Home. Her impressive skills span a wide spectrum of psychological disciplines, including Mental Health, Psychotherapy, Psychology, Psychological Assessment, Group Therapy, Counseling Psychology, Clinical Supervision, and Mental Health Counseling.',
-      'In a world where mental health is of utmost importance, Mrs. Swetha KT stands as a beacon of hope and support for those seeking guidance and healing. Her dedication to her craft and her commitment to improving the lives of others make her an invaluable asset to the field of psychology. Through her work, she helps individuals navigate the complexities of their minds and find the path to mental and emotional well-being. Swetha KT is a true advocate for mental health, and her contributions are a testament to her passion for making a positive difference in the lives of those she serves.',
+      '<b>, Puneeth Suraana</b> is a dynamic and accomplished individual whose impact resonates through various avenues, notably as the producer and host of The Galata Podcast. Through his influential talks on The Galata Podcast, Puneeth has emerged as a motivational prodigy, inspiring young minds to embark on their journeys early and make a significant mark in the world. The podcast, aptly named after the Kannada word "Galata," meaning the noise caused by a ruckus, features insightful interviews that delve into the intricacies of entrepreneurship, team building, and the pursuit of creating value for others. With 65+ episodes, Puneeth has interviewed notable figures such as Nikhil Kamath, Ankur Warikoo and Rohith Bhat. His entrepreneurial prowess is evident in his previous venture, Swaggers, a reliable and affordable clothing startup that gained prominence on college campuses and even secured a presence in the IPL ecosystem. Puneeth achievements include winning a national award for being one of the youngest entrepreneurs in India and being appointed as the brand ambassador of Fair and Handsome alongside Hrithik Roshan. His diverse interests extend to classical Carnatic music and Argentine Tango, and he is a veteran Toastmaster. Puneeth's journey is marked by accolades, entrepreneurial accomplishments, and a commitment to inspiring others to make some Galata.',
     ],
     show_after: '2023-10-04T09:30:00+000',
-    img_link: 'swe.jpg',
+    img_link: 'Puneeth.jpg',
   },
   {
-    name: 'Mrs. Vinatha K',
-    title: 'Associate Professor and HOD',
+    name: 'Bijoy Venugopal',
+    title: 'Storyteller & Educator',
     description: [
-      "<b>Mrs. Vinatha K</b>, is an Associate Professor and HOD of the Pschology department at the prestigious St. Agnes University, Mangalore. As a dedicated psychologist, she has delved deep into the human mind, unraveling its intricacies and offering profound insights into our behavior. Her passion for teaching has ignited a spark in countless students, guiding them toward a deeper understanding of themselves and the world around them.",
-      "Mrs. Vinatha is a published author in addition to being a teacher. Her scholarly work has received praise from the academic community and is a testament to her dedication to advancing our understanding of human nature. Her book is a guiding light for knowledge, illuminating the complexities that make us wholly human. Mrs. Vinatha K. is an accomplished speaker in addition to being a prolific writer. She has spoken on numerous stages, captivating audiences with her speeches on leadership and human behavior. Her messages strike a deep chord with her listeners, igniting discussions that result in transformation and personal growth.",
-      'She has the charisma of a storyteller and the wisdom of a sage, and is bound to whisk us away on a journey through the fascinating realms of psychology and leadership through her captivating talk.',
+      "<b>Bijoy Venugopal</b>, an independent communications consultant with over two decades' experience in media and corporate communications, Bijoy Venugopal has diverse expertise in storytelling, content strategy and leading editorial teams. BEEJ WORD STUDIO, his independent consultancy, is dedicated to telling inclusive, pluralistic, and empowering human stories. He works with nonprofits, educational institutions, and startups, crafting compelling narratives across various media platforms. Bijoy has left his mark on leading internet companies such as Flipkart and Yahoo!, as well as startups, indie rock bands, NGOs, and infotech giants. He has worn many hats, including those of a cartoonist, journalist, travel writer and editor, communications leader and educator. Bijoy Venugopal's work in corporate communications, travel writing and long-form storytelling have earned him accolades, establishing him as a distinguished professional in the field. His passion extends to other areas of interest, as he strives to find purpose in building communities around responsible and sustainable travel, nature education, citizen science for awareness and conservation of urban wildlife, and birding.
     ],
     show_after: '2023-10-04T09:30:00+000',
-    img_link: 'vina.jpg',
+    img_link: 'Bijoy.jpg',
   },
   
 ];

257ebd94f7ce190ea8dcbb72d96dffa05e00c1fe
Violina Doley <97302655+violinadoley@users.noreply.github.com>
2024-01-28 12:49:06 +0530

    Update speakers.js

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,59 +1,59 @@
 var speakers = [
   
-  {
-    name: 'Gita Balakrishnan',
-    title: 'Architect',
-    description: [
-      'An extraordinary architect, Gita Balakrishnan, approaches her craft uniquely by going beyond traditional structures to build stories, minds, and ideas. Since laying the foundation of Ethos in 2002, she has been dedicated to raising awareness about the built environment, supporting aspiring professionals, and creating a network of future leaders in design and engineering. While her works have earned her widespread recognition, many do not know of some of the darkest moments in her life that she overcame to be where she is today. She has chosen to open up to the audience today on her personal life, priorities, setbacks and rising back. Her impactful contributions from her early career days extend to various community-driven initiatives, including various slum redevelopment programs as a part of her role at AVAS. This multifaceted engagement underscores her passion for positive change in community development and urban living, reflecting a dedicated effort to contribute meaningfully to various projects. From her vivid portfolio in the two decades since, her most recent highlight is the "Walk for a Cause" initiative wherein she has been undertaking long distance walks between cities, states and even across borders to spotlight the importance of responsible design and the contributions of the Architecture and Design community to society. Every step in this journey contributes to building a better, more consciously designed future.',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '1.png',
-  },
-  {
-    name: 'Rhythm',
-    title: 'Guitarist',
-    description: [
-      'A 28-year-old Mumbai-based guitarist, Rhythm has been hailed a prodigy from a very young age, often getting coverage and accolades from his peers at school and college. Rhythm is also a classically trained tabla player. Using the guitar as an extension and having technical proficiency makes him a successful musician ranging across multiple genres. Rhythm has toured Germany and South America and collaborated with names like A.R Rahman, Gino banks, and Zakir Hussian.',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '2.png',
-  },
-  {
-    name: 'Meera Valenkar',
-    title: 'Cyclist',
-    description: [
-      'Meera Velankar is a resilient and accomplished individual with a Ph.D. in Life Sciences. Over the past decade and more , she accidentally transitioned into an endurance cyclist, defying conventional expectations and carving a unique path in the world of cycling. Her accidental entry into cycling in 2011 became a transformative journey, encompassing racing, triathlons, and self-supported long-distance events. Her cycling journey, averaging an impressive 8,000 km annually, symbolizes not just a sport but a transformative means of overcoming identity crises and embracing unforeseen passions. The years from 2015 to 2023 witnessed. Her notable cycling expeditions, including a Guinness World Record, and the completion of challenging routes in India amid the complexities of three corona waves. Of particular significance is her tandem cycling expedition to Umlingla in 2023, an exploration of high-altitude riding that embodies the essence of living life fruitfully and embracing challenges. Her story resonates with the belief that life challenges are opportunities for growth. Her experiences underscore the importance of continuous self-challenge and a commitment to embrace daily challenges as stepping stones to a fulfilling life.',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '3.png',
-  },
-  {
-    name: 'Sonali Swali',
-    title: 'Cyclist',
-    description: [
-      'As an International Award Winning Fitness Athlete and Asians Championship BRONZE Medalist, Sonali Swami has achieved significant milestones in her athletic journey. Beyond her prowess in fitness, Sonali is a certified Zumba and Bokwa Instructor, a TRX suspension Trainer, and a Les Mills Body Pump expert, showcasing her multifaceted expertise in the fitness domain. In addition to her athletic achievements, Sonali is also a trained Kathak dancer, demonstrating her commitment to the performing arts. As an independent business owner and entrepreneur, she exemplifies versatility and resilience in balancing her diverse roles. Her commitment to excellence and her ability to excel in various fields underscore her dedication to continuous personal and professional growth.',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '4.png',
-  },
-  {
-    name: '',
-    title: '',
-    description: [
-      '',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '5.png',
-  },
-  {
-    name: 'Dhyvik G J',
-    title: 'CEO of Changepay',
-    description: [
-      'Dhyvik G J, Founder, and CEO of Changepay,  emerges as a dynamic force in the entrepreneurial landscape. His journey is marked by remarkable achievements, beginning with securing Angel investment even before completing his graduation. Under his leadership, Changepay has carved a niche by digitizing college commerce activities, providing students with a seamless platform to access their needs conveniently delivered to their doorstep. His strategic vision and hands-on approach have propelled Changepay to the forefront of the industry, making a significant impact on the way college communities engage in commerce. His entrepreneurial journey reflects a relentless pursuit of goals and a willingness to embrace challenges. As a visionary leader, Dhyvik continues to inspire by turning dreams into reality and shaping the future of commerce for the next generation.',
-    ],
-    show_after: '2023-10-04T09:30:00+000',
-    img_link: '6.png',
-  },
+  // {
+  //   name: 'Gita Balakrishnan',
+  //   title: 'Architect',
+  //   description: [
+  //     'An extraordinary architect, Gita Balakrishnan, approaches her craft uniquely by going beyond traditional structures to build stories, minds, and ideas. Since laying the foundation of Ethos in 2002, she has been dedicated to raising awareness about the built environment, supporting aspiring professionals, and creating a network of future leaders in design and engineering. While her works have earned her widespread recognition, many do not know of some of the darkest moments in her life that she overcame to be where she is today. She has chosen to open up to the audience today on her personal life, priorities, setbacks and rising back. Her impactful contributions from her early career days extend to various community-driven initiatives, including various slum redevelopment programs as a part of her role at AVAS. This multifaceted engagement underscores her passion for positive change in community development and urban living, reflecting a dedicated effort to contribute meaningfully to various projects. From her vivid portfolio in the two decades since, her most recent highlight is the "Walk for a Cause" initiative wherein she has been undertaking long distance walks between cities, states and even across borders to spotlight the importance of responsible design and the contributions of the Architecture and Design community to society. Every step in this journey contributes to building a better, more consciously designed future.',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '1.png',
+  // },
+  // {
+  //   name: 'Rhythm',
+  //   title: 'Guitarist',
+  //   description: [
+  //     'A 28-year-old Mumbai-based guitarist, Rhythm has been hailed a prodigy from a very young age, often getting coverage and accolades from his peers at school and college. Rhythm is also a classically trained tabla player. Using the guitar as an extension and having technical proficiency makes him a successful musician ranging across multiple genres. Rhythm has toured Germany and South America and collaborated with names like A.R Rahman, Gino banks, and Zakir Hussian.',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '2.png',
+  // },
+  // {
+  //   name: 'Meera Valenkar',
+  //   title: 'Cyclist',
+  //   description: [
+  //     'Meera Velankar is a resilient and accomplished individual with a Ph.D. in Life Sciences. Over the past decade and more , she accidentally transitioned into an endurance cyclist, defying conventional expectations and carving a unique path in the world of cycling. Her accidental entry into cycling in 2011 became a transformative journey, encompassing racing, triathlons, and self-supported long-distance events. Her cycling journey, averaging an impressive 8,000 km annually, symbolizes not just a sport but a transformative means of overcoming identity crises and embracing unforeseen passions. The years from 2015 to 2023 witnessed. Her notable cycling expeditions, including a Guinness World Record, and the completion of challenging routes in India amid the complexities of three corona waves. Of particular significance is her tandem cycling expedition to Umlingla in 2023, an exploration of high-altitude riding that embodies the essence of living life fruitfully and embracing challenges. Her story resonates with the belief that life challenges are opportunities for growth. Her experiences underscore the importance of continuous self-challenge and a commitment to embrace daily challenges as stepping stones to a fulfilling life.',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '3.png',
+  // },
+  // {
+  //   name: 'Sonali Swali',
+  //   title: 'Cyclist',
+  //   description: [
+  //     'As an International Award Winning Fitness Athlete and Asians Championship BRONZE Medalist, Sonali Swami has achieved significant milestones in her athletic journey. Beyond her prowess in fitness, Sonali is a certified Zumba and Bokwa Instructor, a TRX suspension Trainer, and a Les Mills Body Pump expert, showcasing her multifaceted expertise in the fitness domain. In addition to her athletic achievements, Sonali is also a trained Kathak dancer, demonstrating her commitment to the performing arts. As an independent business owner and entrepreneur, she exemplifies versatility and resilience in balancing her diverse roles. Her commitment to excellence and her ability to excel in various fields underscore her dedication to continuous personal and professional growth.',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '4.png',
+  // },
+  // {
+  //   name: '',
+  //   title: '',
+  //   description: [
+  //     '',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '5.png',
+  // },
+  // {
+  //   name: 'Dhyvik G J',
+  //   title: 'CEO of Changepay',
+  //   description: [
+  //     'Dhyvik G J, Founder, and CEO of Changepay,  emerges as a dynamic force in the entrepreneurial landscape. His journey is marked by remarkable achievements, beginning with securing Angel investment even before completing his graduation. Under his leadership, Changepay has carved a niche by digitizing college commerce activities, providing students with a seamless platform to access their needs conveniently delivered to their doorstep. His strategic vision and hands-on approach have propelled Changepay to the forefront of the industry, making a significant impact on the way college communities engage in commerce. His entrepreneurial journey reflects a relentless pursuit of goals and a willingness to embrace challenges. As a visionary leader, Dhyvik continues to inspire by turning dreams into reality and shaping the future of commerce for the next generation.',
+  //   ],
+  //   show_after: '2023-10-04T09:30:00+000',
+  //   img_link: '6.png',
+  // },
   // {
   //   name: ' ',
   //   title: ' ',

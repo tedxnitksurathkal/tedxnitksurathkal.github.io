05da8f2dcae31e6254a8b9dae2908507d68da356
Violina Doley <97302655+violinadoley@users.noreply.github.com>
2024-02-01 19:47:23 +0530

    Update speakers.js

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,86 +1,86 @@
 var speakers = [
   
-  // {
-  //   name: 'Gita Balakrishnan',
-  //   title: 'Architect',
-  //   description: [
-  //     'An extraordinary architect, Gita Balakrishnan, approaches her craft uniquely by going beyond traditional structures to build stories, minds, and ideas. Since laying the foundation of Ethos in 2002, she has been dedicated to raising awareness about the built environment, supporting aspiring professionals, and creating a network of future leaders in design and engineering. While her works have earned her widespread recognition, many do not know of some of the darkest moments in her life that she overcame to be where she is today. She has chosen to open up to the audience today on her personal life, priorities, setbacks and rising back. Her impactful contributions from her early career days extend to various community-driven initiatives, including various slum redevelopment programs as a part of her role at AVAS. This multifaceted engagement underscores her passion for positive change in community development and urban living, reflecting a dedicated effort to contribute meaningfully to various projects. From her vivid portfolio in the two decades since, her most recent highlight is the "Walk for a Cause" initiative wherein she has been undertaking long distance walks between cities, states and even across borders to spotlight the importance of responsible design and the contributions of the Architecture and Design community to society. Every step in this journey contributes to building a better, more consciously designed future.',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '1.png',
-  // },
-  // {
-  //   name: 'Rhythm',
-  //   title: 'Guitarist',
-  //   description: [
-  //     'A 28-year-old Mumbai-based guitarist, Rhythm has been hailed a prodigy from a very young age, often getting coverage and accolades from his peers at school and college. Rhythm is also a classically trained tabla player. Using the guitar as an extension and having technical proficiency makes him a successful musician ranging across multiple genres. Rhythm has toured Germany and South America and collaborated with names like A.R Rahman, Gino banks, and Zakir Hussian.',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '2.png',
-  // },
-  // {
-  //   name: 'Meera Valenkar',
-  //   title: 'Cyclist',
-  //   description: [
-  //     'Meera Velankar is a resilient and accomplished individual with a Ph.D. in Life Sciences. Over the past decade and more , she accidentally transitioned into an endurance cyclist, defying conventional expectations and carving a unique path in the world of cycling. Her accidental entry into cycling in 2011 became a transformative journey, encompassing racing, triathlons, and self-supported long-distance events. Her cycling journey, averaging an impressive 8,000 km annually, symbolizes not just a sport but a transformative means of overcoming identity crises and embracing unforeseen passions. The years from 2015 to 2023 witnessed. Her notable cycling expeditions, including a Guinness World Record, and the completion of challenging routes in India amid the complexities of three corona waves. Of particular significance is her tandem cycling expedition to Umlingla in 2023, an exploration of high-altitude riding that embodies the essence of living life fruitfully and embracing challenges. Her story resonates with the belief that life challenges are opportunities for growth. Her experiences underscore the importance of continuous self-challenge and a commitment to embrace daily challenges as stepping stones to a fulfilling life.',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '3.png',
-  // },
-  // {
-  //   name: 'Sonali Swali',
-  //   title: 'Cyclist',
-  //   description: [
-  //     'As an International Award Winning Fitness Athlete and Asians Championship BRONZE Medalist, Sonali Swami has achieved significant milestones in her athletic journey. Beyond her prowess in fitness, Sonali is a certified Zumba and Bokwa Instructor, a TRX suspension Trainer, and a Les Mills Body Pump expert, showcasing her multifaceted expertise in the fitness domain. In addition to her athletic achievements, Sonali is also a trained Kathak dancer, demonstrating her commitment to the performing arts. As an independent business owner and entrepreneur, she exemplifies versatility and resilience in balancing her diverse roles. Her commitment to excellence and her ability to excel in various fields underscore her dedication to continuous personal and professional growth.',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '4.png',
-  // },
-  // {
-  //   name: '',
-  //   title: '',
-  //   description: [
-  //     '',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '5.png',
-  // },
-  // {
-  //   name: 'Dhyvik G J',
-  //   title: 'CEO of Changepay',
-  //   description: [
-  //     'Dhyvik G J, Founder, and CEO of Changepay,  emerges as a dynamic force in the entrepreneurial landscape. His journey is marked by remarkable achievements, beginning with securing Angel investment even before completing his graduation. Under his leadership, Changepay has carved a niche by digitizing college commerce activities, providing students with a seamless platform to access their needs conveniently delivered to their doorstep. His strategic vision and hands-on approach have propelled Changepay to the forefront of the industry, making a significant impact on the way college communities engage in commerce. His entrepreneurial journey reflects a relentless pursuit of goals and a willingness to embrace challenges. As a visionary leader, Dhyvik continues to inspire by turning dreams into reality and shaping the future of commerce for the next generation.',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '6.png',
-  // },
-  // {
-  //   name: ' ',
-  //   title: ' ',
-  //   description: [
-  //     '',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '7.png',
-  // },
-  // {
-  //   name: ' ',
-  //   title: ' ',
-  //   description: [
-  //     ' ',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '7.png',
-  // },
-  // {
-  //   name: ' ',
-  //   title: ' ',
-  //   description: [
-  //     ' ',
-  //   ],
-  //   show_after: '2023-10-04T09:30:00+000',
-  //   img_link: '7.png',
-  // },
+  {
+    name: 'Meera Valenkar',
+    title: 'Cyclist',
+    description: [
+      'Meera Velankar is a resilient and accomplished individual with a Ph.D. in Life Sciences. Over the past decade and more , she accidentally transitioned into an endurance cyclist, defying conventional expectations and carving a unique path in the world of cycling. Her accidental entry into cycling in 2011 became a transformative journey, encompassing racing, triathlons, and self-supported long-distance events. Her cycling journey, averaging an impressive 8,000 km annually, symbolizes not just a sport but a transformative means of overcoming identity crises and embracing unforeseen passions. The years from 2015 to 2023 witnessed. Her notable cycling expeditions, including a Guinness World Record, and the completion of challenging routes in India amid the complexities of three corona waves. Of particular significance is her tandem cycling expedition to Umlingla in 2023, an exploration of high-altitude riding that embodies the essence of living life fruitfully and embracing challenges. Her story resonates with the belief that life challenges are opportunities for growth. Her experiences underscore the importance of continuous self-challenge and a commitment to embrace daily challenges as stepping stones to a fulfilling life.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '1pic.png',
+  },
+  {
+    name: 'Gita Balakrishnan',
+    title: 'Architect',
+    description: [
+      'An extraordinary architect, Gita Balakrishnan, approaches her craft uniquely by going beyond traditional structures to build stories, minds, and ideas. Since laying the foundation of Ethos in 2002, she has been dedicated to raising awareness about the built environment, supporting aspiring professionals, and creating a network of future leaders in design and engineering. While her works have earned her widespread recognition, many do not know of some of the darkest moments in her life that she overcame to be where she is today. She has chosen to open up to the audience today on her personal life, priorities, setbacks and rising back. Her impactful contributions from her early career days extend to various community-driven initiatives, including various slum redevelopment programs as a part of her role at AVAS. This multifaceted engagement underscores her passion for positive change in community development and urban living, reflecting a dedicated effort to contribute meaningfully to various projects. From her vivid portfolio in the two decades since, her most recent highlight is the "Walk for a Cause" initiative wherein she has been undertaking long distance walks between cities, states and even across borders to spotlight the importance of responsible design and the contributions of the Architecture and Design community to society. Every step in this journey contributes to building a better, more consciously designed future.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '2pic.png',
+  },
+  {
+    name: 'Rhythm',
+    title: 'Guitarist',
+    description: [
+      'A 28-year-old Mumbai-based guitarist, Rhythm has been hailed a prodigy from a very young age, often getting coverage and accolades from his peers at school and college. Rhythm is also a classically trained tabla player. Using the guitar as an extension and having technical proficiency makes him a successful musician ranging across multiple genres. Rhythm has toured Germany and South America and collaborated with names like A.R Rahman, Gino banks, and Zakir Hussian.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '3pic.png',
+  },
+  {
+    name: 'Dhyvik G J',
+    title: 'CEO of Changepay',
+    description: [
+      'Dhyvik G J, Founder, and CEO of Changepay,  emerges as a dynamic force in the entrepreneurial landscape. His journey is marked by remarkable achievements, beginning with securing Angel investment even before completing his graduation. Under his leadership, Changepay has carved a niche by digitizing college commerce activities, providing students with a seamless platform to access their needs conveniently delivered to their doorstep. His strategic vision and hands-on approach have propelled Changepay to the forefront of the industry, making a significant impact on the way college communities engage in commerce. His entrepreneurial journey reflects a relentless pursuit of goals and a willingness to embrace challenges. As a visionary leader, Dhyvik continues to inspire by turning dreams into reality and shaping the future of commerce for the next generation.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '4pic.jpg',
+  },
+  {
+    name: '',
+    title: '',
+    description: [
+      '',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '5.png',
+  },
+  {
+    name: 'Gowri Shankar',
+    title: 'Wildlife Biologist',
+    description: [
+      'Renowned wildlife biologist and TED speaker Dr. Gowri Shankar has contributed significantly to King cobra biology and its evolution. He  is  a TED Fellow in 2020 and speaker, he received accolades such as the ‘Herpetologist of the Year 2015’ from the Swedish Herpetological Society and the ‘Vasundhara Mitra Award 2023’ from Kirloskar Ferrous Industries Limited. Co-Founder and Director of Kalinga Foundation and  Kalinga Center for Rainforest Ecology. He  recently challenged a longstanding belief, revealing the existence of four king cobra species as his PhD. His PhD. research delves into the phylogeography of king cobras across Southeast Asia. He had many authored and co-authored  scientific papers and book reviews to his credit. Gowri Shankar is a familiar face in international wildlife documentaries, having featured in productions like ‘King Cobra and I,’ ‘Secrets of the King Cobra,’ ‘One million snake bites,’ and more, showcased on BBC, Discovery, NatGeo Wild, Animal Planet, Smithsonian, and the National Geographic Channel. With a wealth of experience, he previously worked at esteemed institutions, including the Center for Herpetology at Madras Crocodile Bank Trust, and played a crucial role in establishing the Agumbe Rainforest Research Station in Karnataka. Having rescued and relocated over 400 king cobras, monitored 50 nests, and released 500 hatchlings, Gowri was pivotal in pioneering radio telemetry studies, unveiling the mysterious life of king cobras His dedication extends to outreach programs, emphasizing wildlife conservation to the masses',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '6pic.png',
+  },
+  {
+    name: 'Gaurang Sanghvi',
+    title: 'Personal Finance Content Creator',
+    description: [
+      'Gaurang Sanghvi known for his personal finance content and work in the  financial services sector.  His professional journey panning over multiple companies at leading organizations such as DSP Mutual Funds, Tala, RBL Bank and Instakash. His ability to demystify complex financial concepts, making them accessible and understandable to the average person sets him apart. His approach is straightforward yet thorough, ensuring that financial literacy is not just for the experts. With a passion for making a meaningful difference, Gaurang has committed himself to a objective: guiding 10,000 working professionals toward achieving their financial goals, focusing particularly on helping them start their journey to reach a milestone of 1 Crore. His mission is about fostering financial growth.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '7pic.png',
+  },
+  {
+    name: "Flexcia D''souza",
+    title: 'Digital Creator and Travel Enthusiast',
+    description: [
+      'Flexcia D''souza, a seasoned communications and media professional with over 12 years of experience, began her journey at the Times of India in 2011. From full-time roles at Goa Times and Times Life to freelancing as a food and travel writer, Flexcia''s diverse expertise spans tourism, nightlife, food, and entertainment. As a motorcycle enthusiast who loves riding solo, Flexcia adds an adventurous touch to her journey. Her rides reflect courage, independence, and a fearless spirit to embrace the open road. She''s created a niche for herself as a travel content creator on Instagram. She was recognized as the Offbeat Lifestyle Influencer of the Year 2022 and Travel Trailblazer of the Year 2023. Flexcia isn''t just a traveler; she''s a storyteller. Her visuals weave narratives that inspire a global community to embark on their own extraordinary journeys. Collaborating with notable global and Indian brands, Flexcia''s influence extends beyond travel, making her a prominent figure in the lifestyle space. With years of media experience, she consults with brands in the hospitality and alco-bev space for social media marketing. Flexcia''s journey, marked by adaptability and success in the media and communications realm, makes her an engaging storyteller. She brings a wealth of experience, promising to inspire and captivate our audience with her insights.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '8pic.jpg',
+  },
+  {
+    name: 'Varun Painter',
+    title: 'Automative Journalist and Motorcyclist',
+    description: [
+      'Varun Painter, a prominent figure in India''s automotive journalism, embarked on his journey with motorcycles, inspired by family experiences on the KB100 and AX100. As a seasoned motorcyclist and video presenter, Varun joined PowerDrift, where he has been a beloved anchor for almost seven years. Despite not having a specific idol in automotive journalism, he acknowledges the influence of exceptional individuals he has encountered during his career. Varun''s dual role as a motorcyclist and anchor at PowerDrift involves intricate behind-the-scenes work. He emphasizes the significance of teamwork and the camaraderie with colleagues, attributing his success to the collaborative effort that goes into crafting each video. Varun''s narrative isn''t just a chronicle of professional milestones; it''s an inspirational tale of embracing life''s adventures with a positive mindset. His openness to learning, collaborative approach with colleagues, and the ambition to be the best, mirror the Hakuna Matata ethos of facing challenges with resilience and joy. Varun''s story serves as a beacon of inspiration, illustrating how a passion-driven journey, guided by a carefree spirit, can lead to a fulfilling and successful career.',
+    ],
+    show_after: '2023-10-04T09:30:00+000',
+    img_link: '9pic.jpg',
+  },
   
   
 ];
